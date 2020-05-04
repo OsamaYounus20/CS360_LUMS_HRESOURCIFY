5162f4c2f9152b8701abf2f1bbc2f57cfbc8a648
Osama Younus <21100013@lums.edu.pk>
2020-05-04 06:01:28 +0500

    Fixed errors

--- backend/routes/viewInfo.js
+++ backend/routes/viewInfo.js
@@ -11,7 +11,7 @@ connection.connect((err) => {
   if (err) throw err;
   console.log("Connected!");
 });
-var id = 0;
+
 var departments = {};
 connection.query("SELECT * FROM DEPARTMENT", async function (
   error,
@@ -27,29 +27,28 @@ connection.query("SELECT * FROM DEPARTMENT", async function (
     }
   }
 });
+
 exports.display = async function (req, res) {
-  if (req.body.id > 0) {
-    id = req.body.id;
-  }
   connection.query(
     "SELECT * FROM HRUSER WHERE user_id = ?",
-    [id],
+    [req.body.id],
     async function (error, results, fields) {
       if (error) {
         console.log(error);
       } else {
         results = JSON.parse(JSON.stringify(results));
+        // results.shift();
         results.forEach((element) => {
           element.user_id = element.user_id.toString();
           element.department = departments[element.department];
           element.contact_no = element.contact_no.toString();
           element.address = element.address.toString();
           element.dob = element.dob.toString();
+          element.gender = element.gender.toString();
           element.marital_status = element.marital_status.toString();
           element.nationality = element.nationality.toString();
           element.blood_type = element.nationality.toString();
           element.location = element.location.toString();
-
           delete element.manager;
           delete element.user_password;
           delete element.photo;
