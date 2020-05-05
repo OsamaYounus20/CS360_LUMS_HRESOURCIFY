dec08986c176169b59716367e9ba045f0f513e88
OsamaYounus20 <21100013@lums.edu.pk>
2020-05-06 04:59:21 +0500

    added route

--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,18 @@
 var express = require("express");
-var login = require('./routes/login');
-var users = require('./routes/users');
-var addUser= require('./routes/addUser');
-var departments = require('./routes/departments');
-var addDepartment = require('./routes/addDepartment');
-var viewUserInfo = require('./routes/viewUserInfo');
-var viewPersonalInfo = require('./routes/viewPersonalInfo');
-var viewDeptInfo = require('./routes/viewDeptInfo');
-var editUserInfo = require('./routes/editUserInfo');
-var leaves = require('./routes/leaves');
-var viewLeaveRequest = require('./routes/viewLeaveRequest');
-var grantDenyLeave = require('./routes/grantDenyLeave');
-var bodyParser = require('body-parser');
-let cors = require('cors')
+var login = require("./routes/login");
+var users = require("./routes/users");
+var addUser = require("./routes/addUser");
+var departments = require("./routes/departments");
+var addDepartment = require("./routes/addDepartment");
+var viewUserInfo = require("./routes/viewUserInfo");
+var viewPersonalInfo = require("./routes/viewPersonalInfo");
+var viewDeptInfo = require("./routes/viewDeptInfo");
+var editUserInfo = require("./routes/editUserInfo");
+var leaves = require("./routes/leaves");
+var viewLeaveRequest = require("./routes/viewLeaveRequest");
+var grantDenyLeave = require("./routes/grantDenyLeave");
+var bodyParser = require("body-parser");
+let cors = require("cors");
 
 // body parser added
 var app = express();
@@ -20,27 +20,28 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 // Allow cross origin requests
-app.use(cors())
+app.use(cors());
 var router = express.Router();
 
 // test route
-router.get('/', function(req, res) {
-    res.json({ message: 'welcome to our upload module apis' });
+router.get("/", function (req, res) {
+  res.json({ message: "welcome to our upload module apis" });
 });
 
-router.all('/login', login.login);
-router.all('/users', users.display);
-router.all('/add_user', addUser.add);
-router.all('/departments', departments.display);
-router.all('/add_dept', addDepartment.add);
-router.all('/view_user_info', viewUserInfo.display);
-router.all('/view_personal_info', viewPersonalInfo.display);
-router.all('/view_dept_info', viewDeptInfo.display);
-router.all('/edit_user_info', editUserInfo.edit);
-router.all('/leaves', leaves.display);
-router.all('/view_leave_request', viewLeaveRequest.display);
-router.all('/grant_deny_leave', grantDenyLeave.grantDeny);
+router.all("/login", login.login);
+router.all("/users", users.display);
+router.all("/add_user", addUser.add);
+router.all("/departments", departments.display);
+router.all("/add_dept", addDepartment.add);
+router.all("/view_user_info", viewUserInfo.display);
+router.all("/view_personal_info", viewPersonalInfo.display);
+router.all("/edit_personal_info", editUserInfo.edit);
+router.all("/view_dept_info", viewDeptInfo.display);
+router.all("/edit_user_info", editUserInfo.edit);
+router.all("/leaves", leaves.display);
+router.all("/view_leave_request", viewLeaveRequest.display);
+router.all("/grant_deny_leave", grantDenyLeave.grantDeny);
 
-app.use('/api', router);
+app.use("/api", router);
 app.listen(4000);
-console.log('server is listening...');
+console.log("server is listening...");
