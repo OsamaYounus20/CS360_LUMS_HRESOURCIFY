0dd846ee12e3f56946e95853c4138ff75ce3c1ed
AleehaZafar <21100091@lums.edu.pk>
2020-05-05 15:23:21 +0500

    trying ec2 deployment

---
+++ build/precache-manifest.2b9562b4db5e855b73cb6fc20c5bbf27.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "42389585a02b487cec9beaa6eaeb6c2e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d64346bc7ca2032f5bc2",
+    "url": "/static/css/2.8dbe6964.chunk.css"
+  },
+  {
+    "revision": "4c95e4a86e14b6511feb",
+    "url": "/static/css/main.740c0637.chunk.css"
+  },
+  {
+    "revision": "d64346bc7ca2032f5bc2",
+    "url": "/static/js/2.3d8422af.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.3d8422af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4c95e4a86e14b6511feb",
+    "url": "/static/js/main.09a9d37f.chunk.js"
+  },
+  {
+    "revision": "324b029cecec7ef8c8b8",
+    "url": "/static/js/runtime-main.0292b7ab.js"
+  },
+  {
+    "revision": "7940efc40d8e3b477e16cc41b0287139",
+    "url": "/static/media/Poppins-Bold.7940efc4.ttf"
+  },
+  {
+    "revision": "a4e11dda40531debd374e4c8b1dcc7f4",
+    "url": "/static/media/Poppins-Medium.a4e11dda.ttf"
+  },
+  {
+    "revision": "731a28a413d642522667a2de8681ff35",
+    "url": "/static/media/Poppins-Regular.731a28a4.ttf"
+  },
+  {
+    "revision": "37f4f603da9e4bd0f1cc1f3ef15bc888",
+    "url": "/static/media/imagetest.37f4f603.jpg"
+  }
+]);
