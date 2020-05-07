8d35d265e51b8fda353870f33fc8e26a42d26066
HammadArif-developer <61524504+HammadArif-developer@users.noreply.github.com>
2020-05-08 00:32:48 +0500

    Update toDoTaskTable.jsx

--- src/components/toDoTask/toDoTaskTable.jsx
+++ src/components/toDoTask/toDoTaskTable.jsx
@@ -7,7 +7,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import axios from "axios";
-import { Route, withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 
 const columns = [
   { id: "description", label: "Task Name", minWidth: 170, align: "center" },
