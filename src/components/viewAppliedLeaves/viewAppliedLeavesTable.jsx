f2d80fc02968f61a7d0473913eee349e4ac26ccb
OsamaYounus20 <21100013@lums.edu.pk>
2020-05-08 01:10:30 +0500

    connected with backend

--- src/components/viewAppliedLeaves/viewAppliedLeavesTable.jsx
+++ src/components/viewAppliedLeaves/viewAppliedLeavesTable.jsx
@@ -7,25 +7,22 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import axios from "axios";
-import {  withRouter } from "react-router-dom";
+import { withRouter } from "react-router-dom";
 // The Table was borrowed from www.material-ui.com and was altered according to our needs
 
 // Setting Table Columns here
 const columns = [
-{
+  {
     id: "requested_on",
     label: "Requested On",
     minWidth: 170,
     align: "center",
     format: (value) => value.toLocaleString(),
-    },
-  { id: "from_date", 
-    label: "From Date", 
-    minWidth: 170 ,
-    align: "center",},
+  },
+  { id: "start_date", label: "From", minWidth: 170, align: "center" },
   {
-    id: "to_date",
-    label: "To Date",
+    id: "end_date",
+    label: "To",
     minWidth: 170,
     align: "center",
     format: (value) => value.toLocaleString(),
@@ -53,23 +50,25 @@ class viewAppliedLeavesTable extends Component {
     this.state = {
       rows: [],
     };
-    
   }
-//   Receiving data from the backend
+  //   Receiving data from the backend
   componentDidMount() {
     var apiBaseUrl = "http://3.8.136.131:4000/api/";
     var self = this;
     var payload = {
       msg: "Show Previous Leaves Data",
+      id: localStorage.getItem("user_id"),
     };
-    axios.post(apiBaseUrl + "view_applied_leaves", payload).then(function (response) {
-      self.setState({
-        rows: response.data,
+    axios
+      .post(apiBaseUrl + "view_applied_leaves", payload)
+      .then(function (response) {
+        self.setState({
+          rows: response.data,
+        });
       });
-    });
     return;
   }
-//   Displaying the required data using the helper functions defined above. 
+  //   Displaying the required data using the helper functions defined above.
   render() {
     return (
       <Paper>
@@ -91,13 +90,7 @@ class viewAppliedLeavesTable extends Component {
             <TableBody>
               {this.state.rows.map((row) => {
                 return (
-                  <TableRow
-                    hover
-                    role="checkbox"
-                    tabIndex={-1}
-                    key={row.code}
-                    
-                  >
+                  <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
                     {columns.map((column) => {
                       const value = row[column.id];
                       return (
