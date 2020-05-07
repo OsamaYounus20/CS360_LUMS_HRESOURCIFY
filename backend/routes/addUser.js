c77b7026c64d5fb280b1b52af8e23926469da741
HafsaKamran <63223485+HafsaKamran@users.noreply.github.com>
2020-05-07 18:55:51 +0500

    comments

--- backend/routes/addUser.js
+++ backend/routes/addUser.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+
+// setting up a connection with the database
 const connection = mysql.createConnection({
     host: 'database-hrms1.c0sl004lok1n.eu-west-2.rds.amazonaws.com',
     user: 'Aleeha',
@@ -12,6 +14,7 @@ connection.connect((err) => {
     console.log('Connected!');
 });
 
+// a function to generate a random password of length 25 for the new employee
 function generatePassword() {
     var password = '';
     var characterset = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789.,/;:@';
@@ -23,14 +26,16 @@ function generatePassword() {
 }
 
 exports.add = async function(req, res) {
-    if (req.body.Message === 'add user') {
-        user_password = generatePassword();
+    if (req.body.Message === 'add user') {      // if the client sends data to insert a new value in the user table in the database
+        user_password = generatePassword();     // generating a password for the new user
+
+        // querying the database to get the number of users in order to generate the user_id
         connection.query('SELECT COUNT(*) FROM HRUSER', async function(error, results, fields) {
             if (error) {
                 console.log(error);
             } else {
+                // user_id is of the form 20xx
                 user_id = results[0]['COUNT(*)'] + 2000;
-
                 values = [
                     user_id,
                     req.body.Name,
@@ -50,14 +55,17 @@ exports.add = async function(req, res) {
                     req.body.gender,
                 ]
 
+                // querying the database to insert the values into the user table
                 connection.query('INSERT INTO HRUSER (user_id, full_name, user_password, email, contact_no, cnic, dob, marital_status, blood_type, job_title, department, nationality, location, address, manager, gender) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', values, async function(error, results, fields) {
                     if (error) {
+                        // informing the client in case of a server error
                         console.log(error);
                         res.send({
-                            'code': 500,
-                            'success': 'Server error'
+                            'code'    : 500,
+                            'success' : 'Server error'
                         });
                     } else {
+                        // informing the client of success
                         console.log('Employee inserted successfully');
                         res.send({
                             'code': 200,
@@ -67,17 +75,26 @@ exports.add = async function(req, res) {
                 });
             }
         });
-    } else {
-        connection.query('SELECT * FROM DEPARTMENT', async function (error, results, fields) {
+    } else {    // if the client requests a list of departments for the dropdown menu
+        // querying the database to get all departments
+        connection.query('SELECT dept_id, dept_name FROM DEPARTMENT', async function (error, results, fields) {
             if (error) {
+                // informing the client in case of server error
                 console.log(error);
+                res.send({
+                    'code'    : 500,
+                    'success' : 'Server error'
+                })
             } else {
-                console.log('lets send some');
+                // creating a dictionary with department name as the key and department id as the value
+                // this is done because the client enters the name of the department, but the database requires the id
                 var departments = {};
                 results = JSON.parse(JSON.stringify(results));
                 for (var index = 0; index < results.length; index++) {
                     departments[results[index].dept_name] = results[index].dept_id;
                 }
+
+                // sending the dictionary of departments to the client
                 res.send({
                     'code' : 200,
                     'dept' :  departments
--- backend/routes/applyLeave.js
+++ backend/routes/applyLeave.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+
+// setting up a connection with the database
 const connection = mysql.createConnection({
   host: "database-hrms1.c0sl004lok1n.eu-west-2.rds.amazonaws.com",
   user: "Aleeha",
@@ -11,7 +13,9 @@ connection.connect((err) => {
     if (err) throw err;
     console.log("Connected!");
 });
+
 exports.apply = async function (req, res) {
+    // taking substrings of dates in order to convert them into the format required by the database
     values = [
         req.body.userId,
         req.body.currentDate.substring(0,10),
@@ -20,14 +24,19 @@ exports.apply = async function (req, res) {
         req.body.reason,
         req.body.type
     ]
+
+    // querying the database to insert the new leave request
+    // default value of status is 'Pending', so that does not need to be inserted
     connection.query('INSERT INTO LEAVE_REQUEST (requested_by, requested_on, start_date, end_date, reason, type) VALUES (?, ?, ?, ?, ?, ?)', values, async function(error, results, fields) {
         if (error) {
+            // informing the client in case of an error
             console.log(error);
             res.send({
                 'code': 500,
                 'message': 'Server error'
             });
         } else {
+            // informing the client of success
             console.log('applied');
             res.send({
                 'code': 200,
