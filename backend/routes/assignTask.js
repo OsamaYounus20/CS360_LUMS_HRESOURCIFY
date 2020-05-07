2cb7e07b4fd255e585a4f69497efb7644f3c1669
HafsaKamran <63223485+HafsaKamran@users.noreply.github.com>
2020-05-07 18:27:41 +0500

    comments added

--- backend/routes/assignTask.js
+++ backend/routes/assignTask.js
@@ -1,4 +1,6 @@
 const mysql = require("mysql");
+
+// setting up a connection with the database
 const connection = mysql.createConnection({
   host: "database-hrms1.c0sl004lok1n.eu-west-2.rds.amazonaws.com",
   user: "Aleeha",
@@ -13,16 +15,18 @@ connection.connect((err) => {
 });
 
 exports.assign = async function (req, res) {
-    if (req.body.message === 'subordinates') {
-        console.log(req.body);
+    if (req.body.message === 'subordinates') {      // if the client requires a list of subordinates for the dropdown menu
+        // querying the database to get a list of subordinates
         connection.query('SELECT user_id, full_name FROM HRUSER WHERE manager = ?', [req.body.userId], async function(error, results, fields) {
             if (error) {
+                // informing the client in case of a server error
                 console.log(error);
                 res.send({
                     'code' : 500
                 });
             } else {
-                console.log(results);
+                // creating both a dictionary of subordinates with full_name as the key and user_id as value, and a list of subordinate names
+                // this is done because the client will select the name of the subordinate, but the database requires the id
                 results = JSON.parse(JSON.stringify(results));
                 var subordinates = {};
                 var subordinateNames = [];
@@ -30,7 +34,8 @@ exports.assign = async function (req, res) {
                     subordinateNames.push(results[index].full_name);
                     subordinates[results[index].full_name] = results[index].user_id;
                 }
-                console.log('list of subordinates', subordinateNames);
+
+                // sending both the dictionary and the list to the client
                 res.send({
                     'code' : 200,
                     'dict' : subordinates,
@@ -38,7 +43,8 @@ exports.assign = async function (req, res) {
                 })
             }
         });
-    } else {
+    } else {        // if the client is sending data for assigning a new task
+        // substrings of date are taken so they are converted into the format required by the database
         values = [
             req.body.user_id,
             req.body.assigned_to,
@@ -47,16 +53,18 @@ exports.assign = async function (req, res) {
             req.body.description,
             req.body.priority
         ]
-        console.log(values);
 
+        // querying the database to insert the new values in the task table
         connection.query("INSERT INTO TASK (assigned_by, assigned_to, assigned_on, deadline, description, priority) VALUES (?, ?, ?, ?, ?, ?)", values, async function(error, results, fields) {
             if (error) {
+                // informing the client in case of a server error
                 console.log(error);
                 res.send({
                     'code' : 500
             })
             } else {
-                console.log('assigneeeeeeeeeeedddddddddddddd')
+                // informing the client of success
+                console.log('assigned')
                 res.send({
                     'code' : 200
                 })
--- backend/routes/departments.js
+++ backend/routes/departments.js
@@ -1,4 +1,6 @@
 const mysql = require('mysql');
+
+// setting up a connection with the database
 const connection = mysql.createConnection({
     host: 'database-hrms1.c0sl004lok1n.eu-west-2.rds.amazonaws.com',
     user: 'Aleeha',
@@ -13,12 +15,18 @@ connection.connect((err) => {
 });
 
 exports.display = async function (req, res) {
+    // querying the database to get the list of department
     connection.query('SELECT dept_id, dept_name FROM DEPARTMENT LIMIT 10', async function (error, results, fields) {
         if (error) {
+            // informing the client in case of an error
             console.log(error);
+            res.send({
+                'code'    : 500,
+                'success' : 'Server error'
+            });
         } else {
+            // converting the rowdatapacket to a list of dictionaries and sending it to the client
             results = JSON.parse(JSON.stringify(results));
-            console.log(results);
             res.send(results);
         }
     });
