3295e386c68dc9bc4ab645800c28115eb5f1f415
HammadArif-developer <61524504+HammadArif-developer@users.noreply.github.com>
2020-05-07 17:09:05 +0500

    Add files via upload

--- src/components/clock/clock.jsx
+++ src/components/clock/clock.jsx
@@ -3,14 +3,14 @@ import "./clock.css";
 
 /*
 code Borrowed from 
-https://github.com/anil-sidhu/reactjs/blob/master/time_clock_in_react_js.js
+https://github.com/anil-sidhu/reactjs/blob/master/time_clock_in_react_js.js and modified to fit our apss need.
 */
 class clock extends Component {
   constructor() {
     super();
     this.state = { time: new Date() };
   }
-
+//setting state to new time
   currentTime() {
     this.setState({
       time: new Date(),
@@ -30,13 +30,11 @@ class clock extends Component {
                 hour: "2-digit",
                 minute: "2-digit",
               })
-              //   .replace(/(:\d{2}| [AP]M)$/, "")
             }
           </h1>
           <h1 className="dateText">
             {
               this.state.time.toLocaleDateString(navigator.language, {})
-              //   .replace(/(:\d{2}| [AP]M)$/, "")
             }
           </h1>
         </div>
